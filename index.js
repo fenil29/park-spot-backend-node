4da5b0e543d0ddde9d411292b93962dcca39b040
fenil29 <fenilkaneria99@gmail.com>
2020-05-13 23:57:49 +0530

    added api for virtual parking lot

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const uq = require("./user_queries.js");
 const phq = require("./ph_queries.js");
 const parkingq = require("./pl_queries");
+const vpparking = require("./virtual_parking_queries");
 const spotq = require("./spot_queries");
 const store_parking_data = require("./store_parking_data");
 const plhq = require("./plh_queries");
@@ -19,6 +20,16 @@ app.use(
 
 // this function is for storing parking lot data in fms_parking_lot_history table for data analytics
 // store_parking_data();
+
+//allowing cors for all in development
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
@@ -64,6 +75,11 @@ app.delete("/spot/:id", spotq.deleteSD);
 //fms_parking_lot_history
 
 app.post("/lothistory", plhq.getPLHistory);
+
+//for virtual parking lot
+app.get("/vp-parking", vpparking.getPD);
+app.get("/vp-parking/id/:id", vpparking.getPDById);
+app.get("/vp-parkings-pots/:id", vpparking.getPDSpots);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
