656db1949378e81429a4d4fe81e10a3d9268cd61
fenil29 <fenilkaneria99@gmail.com>
2020-07-12 15:34:46 +0530

    add error if not connected to database

--- postgresql_connection.js
+++ postgresql_connection.js
@@ -7,6 +7,13 @@ const connectionString = databaseUrl;
 const pool = new Pool({
   connectionString: connectionString,
 });
+// const client = new Client({
+//   connectionString: connectionString,
+// })
+
+pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+});
 
 module.exports = {
   pool,
